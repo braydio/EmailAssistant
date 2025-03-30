6e375541bb31709df55a604b9cfdd3824935d33f
braydio <chaffee.brayden@gmail.com>
2025-03-29 21:05:48 -0400

    2025-03-29 - config.py - v1.4.0

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ TRASH_DIR = os.getenv("TRASH_DIR", os.path.expanduser("~/.mail/Gmail/Trash"))
 SENT_EMAIL = os.getenv("SENT_EMAIL", "chaffee.brayden@gmail.com")
 
 # Additional directories for mail rules
-IMPORTANT_DIR = os.getenv("IMPORTANT_DIR", os.path.expanduser("~/.mail/Gmail/Important"))
+IMPORTANT_DIR = os.getenv("IMPORTANT_DIR", os.path.expanduser("~/.mail/Gmail/Important/new"))
 SENT_DIR = os.getenv("SENT_DIR", os.path.expanduser("~/.mail/Gmail/Sent"))
 FROMGPT_DIR = os.getenv("FROMGPT_DIR", os.path.expanduser("~/.mail/Gmail/FromGPT"))
 
@@ -26,3 +26,4 @@ LOCAL_AI_BASE_URL = os.getenv("LOCAL_AI_URL")
 # The EverythingLLM API endpoint (e.g., http://192.168.1.239:3001/api/v1)
 ANYTHING_API_URL = os.getenv("ANYTHING_API_URL")
 ANYTHING_API_KEY = os.getenv("ANYTHING_API_KEY")
+
