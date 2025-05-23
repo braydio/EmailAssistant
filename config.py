6c98cdb7d8d78cdf17461b3738e82e1b3228fa48
braydio <chaffee.brayden@gmail.com>
2025-05-22 21:42:01 -0400

    2025-05-22 - config.py - v1.6.0

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 # config.py
 import os
 from dotenv import load_dotenv
@@ -15,15 +14,21 @@ TRASH_DIR = os.getenv("TRASH_DIR", os.path.expanduser("~/.mail/Gmail/Trash"))
 SENT_EMAIL = os.getenv("SENT_EMAIL", "chaffee.brayden@gmail.com")
 
 # Additional directories for mail rules
-IMPORTANT_DIR = os.getenv("IMPORTANT_DIR", os.path.expanduser("~/.mail/Gmail/Important/new"))
+IMPORTANT_DIR = os.getenv(
+    "IMPORTANT_DIR", os.path.expanduser("~/.mail/Gmail/Important/new")
+)
 SENT_DIR = os.getenv("SENT_DIR", os.path.expanduser("~/.mail/Gmail/Sent"))
 FROMGPT_DIR = os.getenv("FROMGPT_DIR", os.path.expanduser("~/.mail/Gmail/FromGPT"))
+
+# IMAP configuration for server-side operations
+IMAP_HOST = os.getenv("IMAP_HOST", "imap.gmail.com")
+IMAP_USER = os.getenv("IMAP_USER")
+IMAP_PASS = os.getenv("IMAP_PASS")
 
 # LLM Configuration
 USE_LOCAL_LLM = "true"
 
 LOCAL_AI_BASE_URL = os.getenv("LOCAL_AI_URL")
-# The EverythingLLM API endpoint (e.g., http://192.168.1.239:3001/api/v1)
 ANYTHING_API_URL = os.getenv("ANYTHING_API_URL")
 ANYTHING_API_KEY = os.getenv("ANYTHING_API_KEY")
 
